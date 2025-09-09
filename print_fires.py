010b9964a86cde5e9f0135f49c09f66e2f0c0dd6
Jared Roth <Jared.l.roth@gmail.com>
2025-09-09 16:36:07 -0600

    Update my_utils.py, print_fires.py, and run.sh to handle errors and introduce __main__ commands

--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,38 @@
+import argparse
 from my_utils import get_column
 
-country='United States of America'
-county_column = 0           # This was originally set to 1, which I think is a mistake
-fires_column = 3            # This was originally set to 4, which I think is a mistake
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column(file_name, county_column, country, fires_column)
-print(fires)
+if __name__ == '__main__':
+
+    # Builder parser object
+    parser = argparse.ArgumentParser(description="Search for the amount of CO2 emissions caused by wildfires in a certain area/region")
+
+    # Now add expected arguments
+    # 1) Country/area
+    parser.add_argument("area", type=str, help="The area to look for the data of interest (default interest: fires)")
+
+    # 2) Country column argument
+    # This selects the column that the function will sift through looking for matches to the Area string
+    parser.add_argument("-cc", "--country-column", type=int, default=0, help="This is the column that will be looked through to find a match for Area. You can switch this column to 1 if you want to search by year, for example.")
+
+    # 3) Fires column argument
+    # This selects the column that has the data of interest 
+    parser.add_argument("-fc", "--fires-column", type=int, default=3, help="This is the column that contains the fire data. You can select a different column to extract different data.")
+
+    # 4) File name argument
+    # This selects the name of the argument
+    parser.add_argument("-fn", "--file-name", type=str, default="Agrofood_co2_emission.csv", help="This is the name of the file that will be looked through")
+
+    # Parse the provided arguments
+    args = parser.parse_args()
+    country = args.area
+    country_column = args.country_column
+    fires_column = args.fires_column
+    file_name = args.file_name
+
+    # Print to make sure we're on track
+    #print("So we've got country, country column, fires column, and file name:")
+    #print(country, " ", country_column, " ", fires_column, " ", file_name)
+
+    # Finally, execute the actual function
+    fires = get_column(file_name, country_column, country, fires_column)
+    print(fires)
--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+echo "First, let's look at fires in the USA:"
+python print_fires.py "United States of America"
+echo "Hey, that worked!"
+echo ""
+
+echo "Next, let's force an error by trying to read a datasheet that doesn't exist:"
+python print_fires.py "Albania" -fn "DoesntExist.csv"
+echo ""
+
+echo "Lastly, let's force another error by forgetting to specify which area we are interested in:"
 python print_fires.py
